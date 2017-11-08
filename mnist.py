5e884442dd486a54855aa5ac721d342449cff7a3
Michał Kapiczyński <mix.kap@gmail.com>
2017-11-08 07:38:22 +0100

    Mnist nc_classify testing with sklearn algorithm

--- mnist.py
+++ mnist.py
@@ -1,23 +1,31 @@
 from loader import MNIST
 from nc_classify import nc_classify
+from sklearn.neighbors.nearest_centroid import NearestCentroid
+import numpy as np
 
 mndata = MNIST('./samples/MNIST/')
 trainingImages, trainingLabels = mndata.load_training()
 testImages, testLabels = mndata.load_testing()
 
 
-def meanValue(data):
-    dataSum = 0
-    for elem in data:
-        dataSum += elem
-    return dataSum / len(data)
+def test_nc_classify_with_sklearn(trainingImages, trainingLabels, testImages, testLabels):
+    X = np.array(trainingImages)
+    y = np.array(trainingLabels)
+    clf = NearestCentroid()
+    clf.fit(X, y)
+
+    correct = 0
+    wrong = 0
+    for index, image in enumerate(testImages):
+        prediction = clf.predict(image)
+        if prediction == testLabels[index]:
+            correct += 1
+        else:
+            wrong += 1
+
+    print("Correct: " + str(correct))
+    print("Wrong: " + str(wrong))
 
 
-def classMeanValue(classData):
-    dataSum = 0
-    for singleImageData in classData:
-        dataSum += meanValue(singleImageData)
-    return dataSum / len(classData)
-
-
-nc_classify(trainingImages, testImages, testLabels, trainingLabels, classMeanValue, meanValue)
+nc_classify(trainingImages, testImages, testLabels, trainingLabels)
+test_nc_classify_with_sklearn(trainingImages, trainingLabels, testImages, testLabels)
