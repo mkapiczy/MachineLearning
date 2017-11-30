1f173ee1251db5ae54e7dc8be294d91ea7996fd8
Michał Kapiczyński <mix.kap@gmail.com>
2017-11-30 21:52:57 +0100

    CrossValidation - bug fix

--- src/algorithms/CrossValidator.py
+++ src/algorithms/CrossValidator.py
@@ -1,10 +1,14 @@
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
-
+import numpy as np
+from random import randint
 
 def cross_val_score(validator, data, target, cv):
+    scores = []
     for i in range(cv):
-        X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.2, random_state=0)
+        randomInt = randint(0, 99)
+        X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.2, random_state=randomInt, stratify=target)
         validator.fit(X_train, y_train)
-        predictions = validator.predictAll(X_test)
-        return accuracy_score(predictions, y_test, normalize=True)
+        predictions = validator.predict(X_test)
+        scores.append(accuracy_score(predictions, y_test, normalize=True))
+    return np.mean(scores)
