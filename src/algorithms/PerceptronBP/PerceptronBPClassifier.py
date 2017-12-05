043ab6f72e49158c68fcfe377ff910462dfe81f7
Michał Kapiczyński <mix.kap@gmail.com>
2017-12-05 23:07:34 +0100

    Perceptron BP - initial weights with 0 Updating proper weight vector

--- src/algorithms/PerceptronBP/PerceptronBPClassifier.py
+++ src/algorithms/PerceptronBP/PerceptronBPClassifier.py
@@ -2,21 +2,22 @@ import numpy as np
 
 from utils import transformLabels
 from random import seed
-from random import random
+import random
 
 
 class PerceptronBPClassifier:
     def __init__(self):
         self.weights = None
         self.nEpoch = 5
-        self.learningRate = 0.05
+        self.learningRate = 0.005
 
     def fit(self, trainingData, trainingLabels):
         self.trainingData = np.array(trainingData)
         self.trainingLabels = np.array(trainingLabels)
         seed(1)
         labels = transformLabels(self.trainingLabels)
-        self.weights = [[random() for i in range(len(trainingData[0]) + 1)] for i in np.unique(trainingLabels)]
+        self.weights = [[0 for i in range(len(trainingData[0]) + 1)] for i in
+                        range(len(np.unique(self.trainingLabels)))]
         # add bias
         self.trainingData = np.insert(self.trainingData, len(self.trainingData[0]), 1, axis=1)
 
@@ -27,9 +28,12 @@ class PerceptronBPClassifier:
                     p = np.dot(np.array(w).transpose(), data)
                     prediction.append(p)
                 predictedLabel = np.argmax(prediction)
+                # print("Predicted : " + str(predictedLabel) + " - " + str(self.trainingLabels[i]))
+                # print(str(prediction))
                 if predictedLabel != self.trainingLabels[i]:
                     error = np.array(labels[i]) - np.array(prediction)
-                    self.weights[predictedLabel] = self.weights[predictedLabel] + self.learningRate * data * -1
+                    # print(str(error))
+                    self.weights[self.trainingLabels[i]] = self.weights[self.trainingLabels[i]] + self.learningRate * data * -1
 
         return self.weights
 
@@ -39,7 +43,7 @@ class PerceptronBPClassifier:
 
     def predictSingle(self, sample):
         sample = np.insert(sample, len(sample), 1)
-        sampleTimesWeight = np.dot(sample, self.weights)
+        sampleTimesWeight = np.dot(self.weights, sample)
         return self.find_nearest(sampleTimesWeight, 1)
 
     def predict(self, data):
