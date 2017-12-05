5c95455d78122e82d872c081daef7d208683ca4a
Michał Kapiczyński <mix.kap@gmail.com>
2017-12-05 21:37:21 +0100

    Working Perceptron MSE implementation with bias

--- src/algorithms/PerceptronMSE/PerceptronMSEClassifier.py
+++ src/algorithms/PerceptronMSE/PerceptronMSEClassifier.py
@@ -1,59 +1,34 @@
 import numpy as np
-import scipy
 
-from algorithms.DataClass import DataClass
-from utils import chunks
+from utils import transformLabels
 
 
 class PerceptronMSEClassifier:
+
     def __init__(self):
         self.weights = None
         self.trainingData = None
         self.trainingLabels = None
 
-
-    def sort_and_deduplicate(self, l):
-        return list(np.unique(sorted(l, reverse=True)))
-
-
-    def pseudo_inverse(self, matrix):
-        print(str(matrix))
-        mTranspose = matrix.transpose()
-        print(str(matrix.transpose()))
-        return np.linalg.inv(np.dot(matrix, mTranspose))
-
-
     def fit(self, trainingData, trainingLabels):
         self.trainingData = np.array(trainingData)
         self.trainingLabels = np.array(trainingLabels)
+        # add bias
+        self.trainingData = np.insert(self.trainingData, len(self.trainingData[0]), 1, axis=1)
 
         pseudoInverse = np.linalg.pinv(self.trainingData)
-        labelsTranspose = self.trainingLabels.transpose()
-        newLabels = []
-        for label in labelsTranspose:
-            newLabels.append(self.to_zero_form(label, 10))
+        labels = transformLabels(self.trainingLabels)
 
-        self.weights = np.dot(pseudoInverse, newLabels)
+        self.weights = np.dot(pseudoInverse, labels)
 
-
-
-    def to_zero_form(self, number, numberOfClasses):
-        array = []
-        for i in range(numberOfClasses):
-            if i == number:
-                array.append(1)
-            else:
-                array.append(-1)
-        return array
-
-    def find_nearest(self, array, value):
+    def findIndexOfNearestElemToValue(self, array, value):
         idx = (np.abs(array - value)).argmin()
         return idx
 
     def predictSingle(self, sample):
-        # weight transpose times sample
-        sampleTimesWeight = np.dot(self.weights.transpose(), sample)
-        return self.find_nearest(sampleTimesWeight, 1)
+        sample = np.insert(sample, len(sample), 1)
+        predictionVector = np.dot(self.weights.transpose(), sample)
+        return self.findIndexOfNearestElemToValue(predictionVector, 1)
 
     def predict(self, data):
         predictions = []
--- src/algorithms/PerceptronMSE/perceptron_mse_test.py
+++ src/algorithms/PerceptronMSE/perceptron_mse_test.py
@@ -1,28 +1,11 @@
-from algorithms.CrossValidator import cross_val_score
 from algorithms.PerceptronMSE.PerceptronMSEClassifier import PerceptronMSEClassifier
-from visual.confusion_matrix import createConfusionMatrix
-import numpy as np
+from visual.confusion_matrix import printCorrectWrong
 
-def validateHyperParameter(trainingData, trainingLabels):
-    clf = PerceptronMSEClassifier()
-    scores = cross_val_score(clf, trainingData, np.array(trainingLabels), cv=5)
-    print(str(scores))
 
 def test_perceptron_mse(trainingData, trainingLabels, testData, testLabels):
     clf = PerceptronMSEClassifier()
     clf.fit(trainingData, trainingLabels)
 
-    correct = 0
-    wrong = 0
-    for index, data in enumerate(testData):
-        prediction = clf.predictSingle(data)
-        if prediction == testLabels[index]:
-            correct += 1
-        else:
-            wrong += 1
-
-    print("Correct: " + str(correct))
-    print("Wrong: " + str(wrong))
-
-    # predictions = clf.predict(testData)
+    predictions = clf.predict(testData)
+    printCorrectWrong(predictions, testLabels)
     # createConfusionMatrix(predictions, testLabels)
